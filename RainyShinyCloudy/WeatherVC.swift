30e55f99b35484309dad24157713b5acc5fae915
n0an <nantog@gmail.com>
2016-10-19 23:02:39 +0300

    start

---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Anton Novoselov on 19/10/2016.
+//  Copyright © 2016 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
